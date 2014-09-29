2c673c32691e2993992076eb729a02e99e86d12d
Zuoning Yin <zuoning@graphsql.com>
2014-09-29 21:36:14 +0000

    update install_python27_ubuntu.sh

--- install_python27_ubuntu.sh
+++ install_python27_ubuntu.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+if which python; then
+    printf "Python already installed.\n"
+    exit 1
+fi
+
 apt-get update
-apt-get -y install make build-essential zlib1g-dev libbz2-dev libreadline-dev libreadline-gplv2-dev libncursesw5-dev tk-dev libgdbm-dev libc6-dev
+apt-get -y install build-essential zlib1g-dev libbz2-dev libreadline-dev libreadline-gplv2-dev libncursesw5-dev tk-dev libgdbm-dev libc6-dev
+apt-get -y install make
 apt-get -y install sqlite3 libsqlite3-dev
 apt-get -y install libssl-dev
 cd Python-2.7.5
 ./configure
+touch Include/Python-ast.h
+touch Python/Python-ast.c
 make
 make install 
