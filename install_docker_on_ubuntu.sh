056bd4ba96ea625c85748703083558fba742b216
Zuoning Yin <zuoning@graphsql.com>
2014-10-14 01:39:28 +0000

    add install_docker_on_ubuntu.sh

---
+++ install_docker_on_ubuntu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# assume ubuntu 14
+apt-get update
+apt-get -y install docker.io
+ln -sf /usr/bin/docker.io /usr/local/bin/docker
+sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+sh -c "echo deb https://get.docker.io/ubuntu docker main\
+> /etc/apt/sources.list.d/docker.list"
+apt-get update
+apt-get -y install lxc-docker
+
+
