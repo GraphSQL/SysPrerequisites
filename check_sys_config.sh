2c4e0815b382124793c6b0642a56ef72039f0629
Yuansen Chen <yuansen.chen@graphsql.com>
2015-02-05 15:19:19 -0800

    Added java version check

--- check_sys_config.sh
+++ check_sys_config.sh
@@ -19,20 +19,19 @@ fi
 
 report="report4GraphSQL_`hostname`.txt"
 (
-#for cmd in gse_canonnical_loader gse_loader ids_worker redis-benchmark redis-check-aof redis-check-dump redis-cli redis-sentinel redis-server ; do  ldd $cmd ; done | sort | awk '{print $1}'|uniq | xargs locate| grep -v debug|sort | xargs rpm -qf |sort |uniq
-
   echo "=== Checking Prerequisites ==="
   echo "**checking required system libraries ..."
   LIBS="glibc libgcc libstdc++ zlib"
   LIBS_MISSING=''
   for lib in $LIBS
   do
-    if $PKGMGR $PKGMGROPT $lib > /dev/null
+    if $PKGMGR $PKGMGROPT $lib > /dev/null 2>&1
     then
-      echo "  $lib found"
+      pkglib=$( $PKGMGR $PKGMGROPT $lib)
+      echo "  \"$lib\" found in $pkglib"
     else
-      echo "  $lib not found"
-      LIBS_MISSING=$LIBS_MISSING $lib
+      echo "  \"$lib\" not found"
+      LIBS_MISSING="$LIBS_MISSING $lib"
     fi
   done
 
@@ -47,6 +46,12 @@ report="report4GraphSQL_`hostname`.txt"
       if [ "j$cmd" = 'jjava' ]
       then
         echo "  $(java -version 2>&1|head -1) found"
+        JV=$(java -version 2>&1|head -1 | grep -oP "[12]\.\d\d*")
+        if [ "$JV" = "1.5" -o "$JV" = "1.6" ]
+        then
+          CMDS_MISSING="$CMDS_MISSING ${cmd}>=1.7"
+          echo "  Java >= 1.7 required"
+        fi
       else
         pkg=$( $PKGMGR $CMDOPT "$(which $cmd)")
         echo "  $cmd found in $pkg"
@@ -61,19 +66,23 @@ report="report4GraphSQL_`hostname`.txt"
   then
     if [ "l$LIBS_MISSING" != 'l' ]
     then
-      echo "The following system libaries are missing: $LIBS_MISSING"
+      echo
+      echo "!! Missing system libaries: $LIBS_MISSING"
     fi
 
     if [ "l$CMDS_MISSING" != 'l' ]
     then
-      echo "The following comands are missing: $CMDS_MISSING"
+      echo
+      echo "!! Missing comand(s): $CMDS_MISSING"
     fi
     
-    echo "Please install the missing items above and run test check again"
+    echo "Please install all missing items and run the check again."
     exit 2
   fi
 
   # not required, but good to know  
+  echo
+  echo "**checking optional commands ..."
   CMDS="python ip netstat make gcc g++"
   for cmd in $CMDS
   do
@@ -110,7 +119,7 @@ report="report4GraphSQL_`hostname`.txt"
   /bin/echo -e "\n---Disk space:" 
     df -h
 
-  /bin/echo -e "\n---NIC and IP" 
+  /bin/echo -e "\n---NIC and IP:" 
     if which ifconfig >/dev/null 2>&1
     then
       ifconfig -a|grep -v '127\|lo' | grep -B1 'inet '
@@ -118,7 +127,7 @@ report="report4GraphSQL_`hostname`.txt"
       ip addr|grep -B1 'inet '
     fi
 
-  /bin/echo -e "\n---Outside Connection"
+  /bin/echo -e "\n---Outside Connection:"
     ping -c 3 -W 3 www.cisco.com
   /bin/echo 
   /bin/echo -e "Please review $report and send it to GraphSQL. Thank you!"
