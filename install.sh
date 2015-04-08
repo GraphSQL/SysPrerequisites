9f93cf7d2f9e3a5b712228b95803ce21ae80668b
yuansen2015 <yuansen.chen@graphsql.com>
2015-04-08 10:47:08 -0700

    reflected changed gium token

--- install.sh
+++ install.sh
@@ -158,7 +158,7 @@ echo "Operating System is $OS"
  	  if has_internet
  	  then
  	    echo "---- Downloading GIUM package ----"
- 	    su - ${GSQL_USER} -c "curl -H 'Authorization: token 3282e34438b0c4b8c0d3e57cbed40312f2d6596a' -L https://api.github.com/repos/GraphSQL/gium/tarball -o gium.tar"
+ 	    su - ${GSQL_USER} -c "curl -H 'Authorization: token 3c6d5d970f58064c59ff6dce31e1782ecc4b6537' -L https://api.github.com/repos/GraphSQL/gium/tarball -o gium.tar"
  	  else
  	    echo "No Internet connection. Please copy GIUM package to $giumtar and rerun the program"
  	    exit 4 
