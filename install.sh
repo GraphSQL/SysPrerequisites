a77f13195f20114ed37563a35d3cfd196e246ea6
yuansen2015 <yuansen.chen@graphsql.com>
2015-10-20 19:01:21 -0700

    [IUM-131] fix typos

--- install.sh
+++ install.sh
@@ -140,7 +140,7 @@ fi
     notice "You may need to run command \"chown -R ${GSQL_USER} ${DATA_PATH}\" "
     sleep 3
  	else
-    progress "Creating folder ${DATA_PATH} ..."
+    progress "Creating folder ${DATA_PATH}"
     mkdir -p ${DATA_PATH}
     chown -R ${GSQL_USER} ${DATA_PATH}
  	fi
@@ -259,7 +259,7 @@ fi
     sed -i -e 's/_warn("Not using mpz_powm_sec/pass #_warn("Not using mpz_powm_sec/' $numberPy
   fi
 
-  if [ ! -d ~$USER_HOME/.python-eggs ]
+  if [ ! -d $USER_HOME/.python-eggs ]
   then
     su - ${GSQL_USER} -c "mkdir ~/.python-eggs; chmod go-w ~/.python-eggs"
   else
@@ -303,4 +303,4 @@ echo
 echo "System prerequisite installation completed."
 echo
 
-echo "You may verify system settings by running \"check_system.sh\" script in SysPrerequisites-master folder."
+echo "You may verify system settings by running \"check_system.sh\" script in SysPrerequisites folder."
