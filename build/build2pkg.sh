9137af00bb4955f583fbedb853d915645e6e8829
pygraphsql <yun.peng@graphsql.com>
2017-01-06 02:04:11 +0000

    [GLIV-197] change dir name

--- build/build2pkg.sh
+++ build/build2pkg.sh
@@ -48,7 +48,7 @@ create_rpm(){
   rpmbuild -ba $rpm_build_dir/SPECS/GraphSQL-syspreq.spec 1>>$LOG 2>&1  
 
   progress "generating the GraphSQL-syspreq package"
-  mkdir -p $rpm_off_repo_dir
+  mkdir -p $rpm_repo_dir
   off_repo="/etc/yum.repos.d/syspreq_off.repo"
   
   if ! rpm -q createrepo >/dev/null 2>&1
@@ -57,41 +57,41 @@ create_rpm(){
 
   echo "[graphsql-local]" > $off_repo
   echo "name=GraphSQL-syspreq Local" >> $off_repo
-  echo "baseurl=file://${rpm_off_repo_dir}" >> $off_repo
+  echo "baseurl=file://${rpm_repo_dir}" >> $off_repo
   echo "gpgcheck=0" >> $off_repo
   echo "enabled=1" >> $off_repo  
 
-  cp $rpm_build_dir/RPMS/x86_64/*.rpm $rpm_off_repo_dir >/dev/null 2>&1
-  createrepo $rpm_off_repo_dir 1>>$LOG 2>&1
+  cp $rpm_build_dir/RPMS/x86_64/*.rpm $rpm_repo_dir >/dev/null 2>&1
+  createrepo $rpm_repo_dir 1>>$LOG 2>&1
 
   progress "generating the GraphSQL-syspreq package with all dependencies"
   if ! rpm -q yum-utils >/dev/null 2>&1
   then yum -y install yum-utils 1>>$LOG 2>&1
   fi
-  total_dir="${rpm_off_repo_dir}/../rpm_off_repo_total"
+  total_dir="${rpm_repo_dir}/../rpm_off_repo_total"
   repotrack -a x86_64 -p $total_dir GraphSQL-syspreq 1>>$LOG 2>&1
   rm -f $total_dir/*.i686.rpm 
   createrepo $total_dir 1>>$LOG 2>&1
 
-  rm -rf $rpm_off_repo_dir
+  rm -rf $rpm_repo_dir
   rm $off_repo
 }
 
 create_deb(){
-  if [ -d $deb_off_repo_dir ]
-  then rm -rf $deb_off_repo_dir
+  if [ -d $deb_repo_dir ]
+  then rm -rf $deb_repo_dir
   fi
-  mkdir -p $deb_off_repo_dir
-  dpkg -b $deb_build_dir/syspreq_deb $deb_off_repo_dir/syspreq_deb.deb 1>>$LOG 2>&1
+  mkdir -p $deb_repo_dir
+  dpkg -b $deb_build_dir/syspreq_deb $deb_repo_dir/syspreq_deb.deb 1>>$LOG 2>&1
   
   if ! dpkg -s dpkg-dev 2>&1 | grep -q 'install ok installed'
   then apt-get -y install dpkg-dev 1>>$LOG 2>&1
   fi
-  newsource="deb file://${deb_off_repo_dir}/ ./"
+  newsource="deb file://${deb_repo_dir}/ ./"
   if ! cat /etc/apt/sources.list | grep "$newsource"
   then echo $newsource >> /etc/apt/sources.list
   fi
-  cd $deb_off_repo_dir
+  cd $deb_repo_dir
   dpkg-scanpackages . /dev/null 1>>$LOG 2>&1 | gzip -9c > Packages.gz 1>>$LOG 2>&1
   apt-get update 1>>$LOG 2>&1
 }
@@ -103,12 +103,12 @@ fi
 OS=$(get_os)
 if [ "Q$OS" = "QRHEL" ]  # Redhat or CentOS
 then
-  rpm_off_repo_dir="${PWD}/rpm_off_repo"
+  rpm_repo_dir="${PWD}/rpm_online_repo"
   rpm_build_dir="${PWD}/rpmbuild"
   create_rpm
   progress "created rpm repository successfully"
 else
-  deb_off_repo_dir="${PWD}/deb_off_repo"
+  deb_repo_dir="${PWD}/deb_online_repo"
   deb_build_dir="${PWD}/dpkgbuild"
   create_deb
   progress "created deb repository successfully"
