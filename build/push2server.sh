0d47177e13199070c5d7892cb5cf8c1d1783842e
pygraphsql <yun.peng@graphsql.com>
2017-01-24 11:10:54 -0800

    [GLIV-197] add push2server.sh

--- build/push2server.sh
+++ build/push2server.sh
@@ -4,11 +4,21 @@ cd `dirname $0`
 source ../prettyprt
 OSG=$(get_os)
 OS=$(echo $OSG | cut -d' ' -f1)
-OSV=$(echo $OSG | cut -d' ' -f2)
-echo $OSG
-echo $OS
-echo $OSV
-if [[ ( "$OS" == "RHEL" && ! -f ../rpm_online_repo.tar.gz ) || ( "$OS" == "UBUNTU" && ! -f ../deb_online_repo.tar.gz ) ]]; then
-  warn "Online repo file does not exist"
-  exit
+os_version=$(echo $OSG | cut -d' ' -f2)
+if [ "$OS" = "RHEL" ]; then
+  name="centos"
+else 
+  name="ubuntu"
 fi
+if [ ! -f "../${name}_${os_version}.tar.gz" ]; then
+  warn "Online repo file does not exist"
+  exit 3
+fi
+scp -i gsql_east.pem "../${name}_${os_version}.tar.gz"  ubuntu@54.83.18.80:/var/www/html/repo
+ssh -i gsql_east.pem ubuntu@54.83.18.80 << EOF
+  cd /var/www/html/repo;
+  rm -rf ${name}_${os_version}
+  tar xzf ${name}_${os_version}.tar.gz
+  rm -f ${name}_${os_version}.tar.gz
+EOF
+
