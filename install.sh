8c86c81821d4fc0480153fcce6372f0711c158a1
pygraphsql <yun.peng@graphsql.com>
2017-01-31 17:42:53 -0800

    [GLIV-197] fixed

--- install.sh
+++ install.sh
@@ -279,16 +279,12 @@ if [ "Q$OS" = "QRHEL" ]; then
   for i in "${arr[@]}"
   do
     if ! rpm -q "${arr[$i]}" >/dev/null 2>&1; then
-      if [ "$OFFLINE" = true ]; then
+      if [ "$ONLINE" = true ]; then
+        yum -y install "${arr[$i]}" 1>>"$LOG" 2>&1
+      fi
+      if ! rpm -q "${arr[$i]}" >/dev/null 2>&1; then
         warn "${arr[$i]} should be installed before GraphSQL installation. Program terminated."
         exit 3
-      else
-        yum -y install "${arr[$i]}"  
-        if [ "$?" -ne "0" ]; then
-          echo "${arr[$i]}"
-          warn "No Internet connection. Program terminated"
-          exit 3 
-        fi
       fi
     fi
   done
@@ -297,15 +293,12 @@ else
   for i in "${arr[@]}"
   do
     if ! dpkg -s ${arr[$i]} 2>&1 | grep -q 'install ok installed'; then
-      if [ "$OFFLINE" = true ]; then
+      if [ "$ONLINE" = true ]; then
+        apy-get -y install "${arr[$i]}" 1>>"$LOG" 2>&1
+      fi
+      if ! dpkg -s ${arr[$i]} 2>&1 | grep -q 'install ok installed'; then
         warn "${arr[$i]} should be installed before GraphSQL installation. Program terminated."
         exit 3
-      else
-        apy-get -y install "${arr[$i]}" 1>>"$LOG" 2>&1
-        if [ "$?" -ne "0" ]; then
-          warn "No Internet connection. Program terminated"
-          exit 3
-        fi
       fi
     fi
   done
